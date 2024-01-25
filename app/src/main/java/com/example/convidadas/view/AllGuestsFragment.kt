8ac33947ad9254adb805a49f5eedaa0c39a4e12b
Sérgio <sergioriosribeiro@outlook.com>
2024-01-25 17:43:50 -0300

    Métodos para listagem das pessoas convidadas

--- app/src/main/java/com/example/convidadas/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidadas/view/AllGuestsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.convidadas.databinding.FragmentAllGuestsBinding
@@ -13,27 +12,32 @@ import com.example.convidadas.viewmodel.AllGuestsViewModel
 class AllGuestsFragment : Fragment() {
 
     private var _binding: FragmentAllGuestsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var viewModel: AllGuestsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val viewModel =
+        viewModel =
             ViewModelProvider(this).get(AllGuestsViewModel::class.java)
 
         _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        viewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        viewModel.getAll()
+
+        observ()
+
         return root
+    }
+
+    private fun observ() {
+        viewModel.guests.observe(viewLifecycleOwner) {
+            val s = ""
+            //Lista de convidados
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/convidadas/viewmodel/AllGuestsViewModel.kt
+++ app/src/main/java/com/example/convidadas/viewmodel/AllGuestsViewModel.kt
@@ -1,13 +1,22 @@
 package com.example.convidadas.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.convidadas.model.GuestModel
+import com.example.convidadas.repository.GuestRepository
 
-class AllGuestsViewModel : ViewModel() {
+class AllGuestsViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is all guests Fragment"
+    private val repository = GuestRepository.getInstance(application.applicationContext)
+
+    private val listAllGuests = MutableLiveData<List<GuestModel>>()
+    val guests: LiveData<List<GuestModel>> = listAllGuests
+
+    fun getAll() {
+        listAllGuests.value = repository.getAll()
     }
-    val text: LiveData<String> = _text
+
 }
